014c89a3dab04175c52a04f5fda894486beede59
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 11:37:54 +0100

    add logincard

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,21 @@
 import Header from "../components/Header/Header";
+import LoginCard from "../components/LoginCard/LoginCard";
+import StudentIcon from "../assets/icons/openmoji_child.svg";
 
 const Login = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <h1 className="login__title">Login</h1>
+      <LoginCard
+        icon={StudentIcon}
+        role={"student"}
+        info1={"Pick an avatar, and monitor"}
+        info2={"your team’s rankings!"}
+      />
+      <LoginCard />
+    </>
+  );
 };
 
 export default Login;
