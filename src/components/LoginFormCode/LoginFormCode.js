1818091aced4d33c504782251ce605fcec8bcf2e
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 13:00:48 +0100

    add student loging page code

--- src/components/LoginFormCode/LoginFormCode.js
+++ src/components/LoginFormCode/LoginFormCode.js
@@ -1,6 +1,14 @@
 import "./LoginFormCode.scss";
+import { useNavigate } from "react-router-dom";
 
 const LoginFormCode = () => {
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate("/chooseavatar");
+  };
+
   return (
     <section className="login-form-code">
       <form className="login-form-code__form">
@@ -15,7 +23,7 @@ const LoginFormCode = () => {
             className="login-form-code__input"
           />
         </div>
-        <button>Enter</button>
+        <button onClick={handleClick}>Enter</button>
       </form>
     </section>
   );
