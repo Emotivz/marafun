c40d3731e22c902727c840184b8469822f23a10a
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 10:59:27 +0100

    add header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import Login from "./pages/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,1 +1,12 @@
 import "./Header.scss";
+import logo from "../../assets/logos/tcslms.svg";
+
+const Header = () => {
+  return (
+    <section className="header__container">
+      <img src={logo} alt="logo" className="header__logo" />
+    </section>
+  );
+};
+
+export default Header;
