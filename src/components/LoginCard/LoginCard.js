419f30d4f9e8314af4720b65dbf580fd6907c91a
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 13:43:40 +0100

    add navigation for login card

--- src/components/LoginCard/LoginCard.js
+++ src/components/LoginCard/LoginCard.js
@@ -1,6 +1,11 @@
 import "./LoginCard.scss";
+import { useNavigate } from "react-router-dom";
 
 const LoginCard = ({ icon, role, info1, info2 }) => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(`/login/${role}`);
+  };
   return (
     <article className="login-card">
       <div className="login-card__icon-wrapper">
@@ -11,7 +16,7 @@ const LoginCard = ({ icon, role, info1, info2 }) => {
         <p className="login-card__info">{info1}</p>
         <p className="login-card__info">{info2}</p>
       </div>
-      <p className="login-card__login-button">
+      <p onClick={handleClick} className="login-card__login-button">
         <span className="login-card__role ">{role} </span>log in
       </p>
     </article>
