abaed093cd9104f36173817890a1d13b2b2f7fc5
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 11:56:36 +0100

    fix browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
 import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,13 +6,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route to={"/"} />
-          <Route to={"/login"} />
+          <Route path={"/"} />
+          <Route path={"/login"} element={<Login />} />
         </Routes>
       </BrowserRouter>
-      <Login />
     </div>
   );
 }
