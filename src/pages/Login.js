a44b65c69bc68d10c226e5c292aeb8f193662aff
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 11:48:15 +0100

    add login card for teacher

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import LoginCard from "../components/LoginCard/LoginCard";
 import StudentIcon from "../assets/icons/openmoji_child.svg";
+import TeacherIcon from "../assets/icons/noto_teacher.svg";
 
 const Login = () => {
   return (
@@ -13,7 +14,12 @@ const Login = () => {
         info1={"Pick an avatar, and monitor"}
         info2={"your team’s rankings!"}
       />
-      <LoginCard />
+      <LoginCard
+        icon={TeacherIcon}
+        role={"teacher"}
+        info1={"Monitor your students and"}
+        info2={"keep an eye on activities"}
+      />
     </>
   );
 };
