71976940663a8ba9966a8f8e78ecc9c16a80436c
Michael Orimolade <siwsher@hotmail.com>
2023-06-16 15:38:58 +0100

    add image

--- src/components/ChooseAvatar/ChooseAvatar.js
+++ src/components/ChooseAvatar/ChooseAvatar.js
@@ -3,6 +3,11 @@ import animalIconURL from "../../assets/logos/Ellipse 12.png";
 import backgroundURL from "../../assets/icons/maxresdefault.jpg";
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import image1 from "../../assets/icons/Ellipse 3.svg";
+import image2 from "../../assets/icons/Ellipse 4.svg";
+import image3 from "../../assets/icons/Ellipse 5.svg";
+import image4 from "../../assets/icons/Ellipse 9.svg";
+import image5 from "../../assets/icons/Ellipse 18.svg";
 
 const randomNames = ["Name1", "Name2", "Name3"];
 
@@ -33,11 +38,11 @@ export default function ChooseAvatar({ setFinalName }) {
       <section className="choose-section">
         <h1 className="choose-section__title">Choose your avatar</h1>
         <div className="choose-section__avatars-container">
-          <img className="choose-section__avatar" src={animalIconURL} alt="" />
-          <img className="choose-section__avatar" src={animalIconURL} alt="" />
-          <img className="choose-section__avatar" src={animalIconURL} alt="" />
-          <img className="choose-section__avatar" src={animalIconURL} alt="" />
-          <img className="choose-section__avatar" src={animalIconURL} alt="" />
+          <img className="choose-section__avatar" src={image1} alt="" />
+          <img className="choose-section__avatar" src={image2} alt="" />
+          <img className="choose-section__avatar" src={image3} alt="" />
+          <img className="choose-section__avatar" src={image4} alt="" />
+          <img className="choose-section__avatar" src={image5} alt="" />
           <img className="choose-section__avatar" src={animalIconURL} alt="" />
           <img className="choose-section__avatar" src={animalIconURL} alt="" />
           <img className="choose-section__avatar" src={animalIconURL} alt="" />
