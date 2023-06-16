81c588e880688f604c024635a85bfef6c0b514ec
Oleksandra Perevozniuk <sashaperevozniuk01@gmail.com>
2023-06-16 11:03:51 +0100

    create component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.scss";
+import ChooseAvatar from "./components/ChooseAvatar/ChooseAvatar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ChooseAvatar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ChooseAvatar/ChooseAvatar.js
@@ -0,0 +1,20 @@
+import "./ChooseAvatar.scss";
+
+export default function ChooseAvatar() {
+  return (
+    <>
+      <section className="choose-section">
+        <h1 className="choose-section__title">Choose your avatar</h1>
+        <div className="choose-section__avatars-container">
+          <img className="choose-section__avatar" src="" alt="" />
+          <img className="choose-section__avatar" src="" alt="" />
+          <img className="choose-section__avatar" src="" alt="" />
+          <img className="choose-section__avatar" src="" alt="" />
+          <img className="choose-section__avatar" src="" alt="" />
+          <img className="choose-section__avatar" src="" alt="" />
+          <img className="choose-section__avatar" src="" alt="" />
+        </div>
+      </section>
+    </>
+  );
+}
